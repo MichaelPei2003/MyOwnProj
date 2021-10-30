bec7a434eddfcb92a1951a3a9e184a79ba2f55f0
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2021-10-30 15:09:50 +0800

    。

---
+++ 课内/20211025/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+/*
+int f(int x)
+{
+    if(x<1){return 0;}
+    if(x==1){return 1;}
+    if(x==2){return 2;}
+    return f(x-1)+f(x-2);
+}
+*/
+
+/*int f(int x)
+{
+    if(x==1){return 1;}
+    else {return x*f(x-1);}
+}
+*/
+
+int f(int x)
+{
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<f(n);
+}
