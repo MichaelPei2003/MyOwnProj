6edcdd849a7e159a486d0882facae97a836108f6
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2021-11-01 11:34:54 +0800

    .

---
+++ 课内/20211101/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    /*int a[10];
+    printf("数组的首地址为%d",a);
+    for(int i=0;i<=9;i++){
+        printf("\n第%d个变量的地址为%d",i,&a[i]);
+    }*/
+    /*int a[10],n,flag,i;
+    for(int i=0;i<=9;i++){
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&n);
+    for(i=0;i<=9;i++){
+        if(a[i]==n){flag=i;printf("\n在a[%d]处找到了数字%d",flag,n);}
+    }*/
+}
