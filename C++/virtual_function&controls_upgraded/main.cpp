1872ab6043e2f6f8408a034985b3edd0c10beca4
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2022-05-06 15:03:20 +0800

    .

--- C++/virtual_function&controls_upgraded/main.cpp
+++ C++/virtual_function&controls_upgraded/main.cpp
@@ -2,13 +2,67 @@
 
 using namespace std;
 
-class CUIControl {
+class CPoint {
+    int x;
+    int y;
 public:
-    virtual void Func() {
-        
-    }
+    CPoint(int a, int b) : x{a}, y{b} {}
+
+    ~CPoint() {}
+};
+
+class CShape {
+public:
+    CShape();
+
+    virtual ~CShape() {}
+
+    virtual bool HitIn() const = 0;
+};
+
+class CRectangle : public CShape {
+    CPoint pos;
+    int width;
+    int height;
+public:
+
+};
+
+class CCircle : public CShape {
+
+};
+
+class CUIControl {
+
+public:
+    CUIControl() {}
+
+    virtual ~CUIControl() {}
+
+    virtual void Click() = 0;
+};
+
+class CButton : public CUIControl {
+    char *op;
+
+public:
+    CButton(char *ctrl) : op{ctrl} {}
+
+    virtual ~CButton() {}
+};
+
+class CMenu : public CUIControl {
+    char *op;
+public:
+    CMenu(char *ctrl) : op{ctrl} {}
+
+    virtual ~CMenu();
 };
 
 int main() {
+    int num, i, j;
+    cin >> num;
+    for (i = 0; i < num; i++) {
 
+    }
 }
