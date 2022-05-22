ca7d22814ead524c35a3da4e8d8cd83561a3d84e
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2022-05-22 14:47:16 +0800

    /

---
+++ C++/virtual_function&controls_upgraded/cmake-build-debug/CMakeFiles/virtual_function_controls_upgraded.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/virtual_function_controls_upgraded.dir/main.cpp.obj"
+  "CMakeFiles/virtual_function_controls_upgraded.dir/main.cpp.obj.d"
+  "libvirtual_function_controls_upgraded.dll.a"
+  "virtual_function_controls_upgraded.exe"
+  "virtual_function_controls_upgraded.exe.manifest"
+  "virtual_function_controls_upgraded.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/virtual_function_controls_upgraded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
